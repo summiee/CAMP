9767cc4b7eb94629c9dbb6b1d44eaf419be4a8fe
Christopher Passow <christopher.passow@desy.de>
2020-04-07 12:52:58 +0200

    example more verbose

--- example/example_timepix_trainIDs.py
+++ example/example_timepix_trainIDs.py
@@ -1,10 +1,12 @@
 from camp.timepix_run import TimePixRun
 from pprint import pprint
 
-run_number = 864  # short run
+run_number = 863  # run with trainIDs cut off at the end
 
 timepix_run = TimePixRun(run_number)
-pprint(timepix_run.__dict__)
+pprint(dir(timepix_run))
+
+print('###')
 
 flash_run_number = timepix_run.get_flash_run_number()
 print(f'corresponding FLASH DAQ run number: {flash_run_number}')
